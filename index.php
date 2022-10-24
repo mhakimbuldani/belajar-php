95a14b890cd4da233282594c8b6e428704ea0d3b
Muhammad Hakim Buldani <mhakimbuldani@gmail.com>
2022-10-24 10:55:36 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Hakim";
+$umur = 20;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Akim";
+$umurAdik = 13;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun"
+
+?>
